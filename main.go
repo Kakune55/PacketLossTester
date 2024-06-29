6a4cc4b6d2ee10d7e53e84b3705d27e2abe5c8a7
Kakune55 <xiao_doubi9637@outlook.com>
2024-06-29 20:46:14 +0800

    feat:添加了多节点选择功能 优化了排版展示 添加了多种数据显示

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func websocketHandler(ws *websocket.Conn) {
 	config := webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{
 			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
+				URLs: []string{"stun:stun.l.google.com:19302","stun:stun01.sipphone.com"},
 			},
 		},
 	}
